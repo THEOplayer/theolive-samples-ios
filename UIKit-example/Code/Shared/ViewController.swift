99a9c416ec8a8d45ac4938e58846a987c50d9303
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-07-10 14:41:44 +0200

    Add cocoapod uikit example

---
+++ UIKit-example/Code/Shared/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UIKitExample
+//
+//  Created by Damiaan Dufaux on 10/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
