b8a70ef5d133d77a62a92805677c05496dde1ba0
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-07-10 17:20:12 +0200

    Add tvOS example

--- UIKit-example/Code/Shared/ViewController.swift
+++ UIKit-example/Code/Shared/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import THEOliveSDK
 
 class ViewController: UIViewController {
-
+    var playerViewController: PlayerViewController?
+    
+    @IBOutlet weak var channelTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for controller in children {
+            if let playerController = controller as? PlayerViewController {
+                playerViewController = playerController
+                break
+            }
+        }
     }
-
+    
+    @IBAction func loadChannelPressed(_ sender: Any) {
+        if let channelID = channelTextField.text {
+            print("Load channelID", channelID)
+            Task {
+                try await playerViewController?.player.loadChannel(channelID)
+            }
+        }
+    }
+    
+    @IBAction func resetChannelPressed(_ sender: Any) {
+        playerViewController?.player.reset()
+    }
 }
-
